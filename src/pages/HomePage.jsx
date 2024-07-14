5d338067f06acc6159a95f3def5b77792c8d7ba8
4lPeReZ <alvaropr05@hotmail.com>
2024-07-14 11:55:21 +0200

    refactor: Update login prompt styling on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -87,11 +87,13 @@ const HomePage = () => {
           />
         </>
       ) : (
-        <div className="text-center">
-          <p className="text-2xl mb-4">Please log in to shorten your links</p>
-          <button onClick={handleLogin} className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded-full shadow-md transition duration-300 w-full md:w-auto mb-4 md:mb-0 md:mr-4">Log In</button>
-          <button onClick={handleRegister} className="bg-green-500 hover:bg-green-700 text-white p-2 rounded-full shadow-md transition duration-300 w-full md:w-auto">Register</button>
-        </div>
+<div className="text-center py-10 px-4 rounded-lg">
+  <h3 className="text-3xl mb-6 text-gray-200 font-bold relative inline-block">
+    Please log in to shorten your links
+    <span className="absolute inset-x-0 -bottom-2 h-1 bg-gradient-to-r from-blue-500 to-purple-500 rounded-md"></span>
+  </h3>
+</div>
+
       )}
     </div>
   );
